1ba1a5c445ce3da6ee469bc2b9ccd6282df2067c
“tmcgraw13” <Timothy.g.mcgraw@gmail.com>
2022-11-30 16:27:18 -0500

    changed port

--- backend/application.py
+++ backend/application.py
@@ -11,12 +11,12 @@ CORS(application, resources={r'/*': {'origins': '*'}})
 
 # sanity check route
 @application.route('/', methods=['GET'])
-def get_random_int():
-    return str("URL works")
+def get_random_str():
+    return "URL works"
 
 @application.route('/ping', methods=['GET'])
 def get_random_int():
     return str(random_int_generator())
 
 if __name__ == '__main__':
-    application.run(port=443)
+    application.run(port=80,debug=True)
