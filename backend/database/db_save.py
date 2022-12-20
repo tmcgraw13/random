1e127e9d7db44d8d8ab07510f01eeae8f6a4b572
millerandy77 <firebolt.andy@gmail.com>
2022-12-19 21:26:40 -0500

    updated string formatting

--- backend/database/db_save.py
+++ backend/database/db_save.py
@@ -3,8 +3,9 @@ from database.db_mydatabases import MyDatabase
 
 def save_random_integer(number):
     database = MyDatabase.numbers
-    sql = ''' INSERT INTO random_tbl (random) VALUES(%s) '''
+    sql = ''' INSERT INTO random_tbl (random) VALUES({0}) '''
     sql_input = sql.format(number)
+    print(sql_input)
 
     # create a database connection and execute input
     execute_input(database,sql_input)
