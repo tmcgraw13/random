fecf3fe505965e0377c6fc6162a26161caf0bb6c
“tmcgraw13” <Timothy.g.mcgraw@gmail.com>
2023-01-06 12:02:02 -0500

    9: Added functional fireworks js package

---
+++ frontend/src/views/Games.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+      <p>Games</p>
+      <h1>Page 4</h1>
+      <TicTacToeVue/>
+    </div>
+  </template>
+  
+  <script>
+  import TicTacToeVue from '@/components/TicTacToeComp/TicTacToe.vue';
+  export default {
+    name: 'Games',
+    components: {TicTacToeVue}
+  };
+  </script>
+  
+  <!-- Add "scoped" attribute to limit CSS to this component only -->
+  <style scoped>
+  
+  </style>
