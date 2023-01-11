a9bc8b6cabbddfed2afc37cb01aff6a9e801c3cc
tmcgraw13 <73917432+tmcgraw13@users.noreply.github.com>
2023-01-10 19:44:09 -0500

    #26 Create User Backend & Database (#28)
    Co-authored-by: “tmcgraw13” <“Timothy.g.mcgraw@gmail.com”>

--- backend/database/db_mydatabases.py
+++ backend/database/db_mydatabases.py
@@ -1,3 +1,9 @@
 class MyDatabase:
     numbers="numbersdb"
+    users="usersdb"
+
+class MyTables:
+    user_tbl="user_tbl"
+    random_tbl="random_tbl"
+
     
---
+++ backend/users/user.py
@@ -0,0 +1,5 @@
+from database.db_save import save_username as user_save
+
+def save_username(input):{
+    user_save(input)
+}
