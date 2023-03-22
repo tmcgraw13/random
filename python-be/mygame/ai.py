2ee9ec6466abc7e9071b357077f460505c652563
“tmcgraw13” <Timothy.g.mcgraw@gmail.com>
2023-03-21 23:18:00 -0400

    chnages

--- python-be/mygame/ai.py
+++ python-be/mygame/ai.py
@@ -15,39 +15,40 @@ class customAI:
                 if board.check_space_before_input(row, col):
                     board_copy = board.copy_board()
                     board_copy.make_move(row,col,'O')
-                    value = self.minimax('O',board_copy,0,False)
+                    value = self.minimax(board_copy,0,False)
+                    print('val',value,'row: ', row+1, 'col: ',col+1)
                     if value > best_value:
                         best_value = value
                         best_move = [row,col]
 
         return best_move
         
-    def minimax(self, currentPlayer: str, board: customBoard, depth, is_maximizing_player):
+    def minimax(self, board: customBoard, depth, is_maximizing_player):
         if board.check_score():
-            if currentPlayer != self.player: # player X
+            if is_maximizing_player: # player X
                 return -1 # Player X Win
             else: # player O
                 return 1
         if not board.check_moves_available():
             return 0
 
-        if is_maximizing_player: # Player O
+        if is_maximizing_player: # Player O Turn
             best_value = -sys.maxsize
             for col in range(3):
                 for row in range(3):
                     if board.check_space_before_input(row, col):
                         board_copy = board.copy_board()
                         board_copy.make_move(row,col,'O')
-                        value = self.minimax('O',board_copy, depth + 1, False)
+                        value = self.minimax(board_copy, depth + 1, False)
                         best_value = max(best_value, value)
             return best_value
-        else: # Player X
+        else: # Player X Turn
             best_value = sys.maxsize
             for col in range(3):
                 for row in range(3):
                     if board.check_space_before_input(row, col):
                         board_copy = board.copy_board()
                         board_copy.make_move(row,col,'X')
-                        value = self.minimax('X',board_copy, depth + 1, True)
+                        value = self.minimax(board_copy, depth + 1, True)
                         best_value = min(best_value, value)
             return best_value
